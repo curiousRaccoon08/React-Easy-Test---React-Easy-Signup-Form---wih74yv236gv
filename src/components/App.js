7e21117f7dc41f4f5fdaaef5bb19d325458e41e7
Satyam teotia <98690287+curiousRaccoon08@users.noreply.github.com>
2022-06-12 21:43:58 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
-  
   return (
-
-  )
-}
-
+    <div className="container">
+      <form>
+        <h1>Login Form</h1>
+        <div className="ui form">
+          <div className="field">
+            {/* <label>Name</label> */}
+            <input
+              id="name"
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
+          </div>
+          <div className="field">
+            {/* <label>Email</label> */}
+            <input id="email" type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            {/* <label>Password</label> */}
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="field">
+            {/* <label>Password</label> */}
+            <input id="consent" type="checkbox" name="consent" />
+          </div>
+          <button id="signup">Signup</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default App;
